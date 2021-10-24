6de63081ac12252acf10111c7519592178bdac39
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-24 10:49:10 +0300

    add a review model class for reviewing project perfomance

--- backend/models.py
+++ backend/models.py
@@ -19,12 +19,35 @@ class Project(models.Model):
     description=models.CharField(max_length=500)
     project_img=models.ImageField(upload_to='images/')
     project_url=models.URLField(max_length=200)
-    projects= models.ForeignKey()
+    user= models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
 
     def __str__(self):
         return self.title
 
     def save_project(self):
         self.save()
+
+class Review(models.Model):
+    Rating=[
+        (1,'1'),
+        (2,'2'),
+        (3,'3'),
+        (4,'4'),
+        (5,'5'),
+        (6,'6'),
+        (7,'7'),
+        (8,'8'),
+        (9,'9'),
+        (10,'10'),
+    ]
+    Design=models.CharField(max_length=100,choices=Rating, default='rate' ),
+    Usability=models.CharField(max_length=100,choices=Rating, default='rate' )
+    Content=models.CharField(max_length=100,choices=Rating, default='rate' )
+    average=models.FloatField(default=0, blank=True)
+
+    def __str__(self):
+        return self.average
+
+    
 
 
