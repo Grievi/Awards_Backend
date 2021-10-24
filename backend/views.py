0c7cba9adccd5b779388402fdb1bc22b77ee3eeb
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-24 11:10:15 +0300

    add class based view named ProfileApi

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from backend.models import Profile, Project,Review
+from backend.serializers import ProfileSerializer, ProjectSerializer, ReviewSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class ProfileApi(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset=Profile.objects.all()
+    serializer_class=ProfileSerializer
+
+
+
