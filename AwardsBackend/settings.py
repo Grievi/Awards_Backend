a265da75502802d0970076379455b5d5a4aaadbe
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-24 09:46:11 +0300

    add serializers.py

--- AwardsBackend/settings.py
+++ AwardsBackend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Awards_BackEnd',
+    'backend',
     'rest_framework',
 
 ]
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'AwardsBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'moringa',
+        'PASSWORD': 'moringa',
     }
 }
 
