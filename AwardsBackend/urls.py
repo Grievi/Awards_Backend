520cc3e5dfed511d2b82d7b69680fe274b2a9ba1
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-26 09:26:47 +0300

    add authentication path that returns auth tokens for access to api's created

--- AwardsBackend/urls.py
+++ AwardsBackend/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from django.contrib import admin
 from backend import views
 from rest_framework import routers
+from rest_framework.authtoken import views as token_views
 
 router=routers.DefaultRouter()
 router.register(r'Profile', views.ProfileApi)
@@ -11,5 +12,6 @@ router.register(r'reviews', views.ReviewApi)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-login/', token_views.obtain_auth_token)
 ]
