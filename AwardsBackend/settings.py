80206b7b4fe0cd33b73ce5242357eb788440a0d5
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-11-01 09:02:49 +0300

    deploying to heroku

--- AwardsBackend/settings.py
+++ AwardsBackend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-sj!-62at3x7y)eg@ae@ef)93f@m!5a1gks23z34c+l5904*rb!
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
